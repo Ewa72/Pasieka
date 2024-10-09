9829dbb35bd68b22061016a037c240a044233072
Ewa72 <ewa07adamus@gmail.com>
2024-10-09 14:02:08 +0200

    09.10.24

--- Produkty/admin.py
+++ Produkty/admin.py
@@ -1,18 +1,20 @@
 from django.contrib import admin
 from .models import Miody
-#from django.utils.html import format_html # do wyswietlenia foto
+from django.utils.html import format_html # do wyswietlenia foto
 
 # Register your models here.
 
 #class ImageAdmin(admin.ModelAdmin):
 
- #   def image_tag(self, obj):
- #       return format_html('<img src="{}" style="max-width:200px; max-height:200px"/>'.format(obj.foto.url))
-
- #   image_tag.short_description = 'foto'
 
 class MiodyAdmin(admin.ModelAdmin):
-    list_display = ("rodzaj", "opis", "foto",)# 'image_tag',)
+ 
+   def image_tag(self, obj):
+       return format_html('<img src="{}" style="max-width:200px; max-height:200px"/>'.format(obj.foto.url))
+   
+   image_tag.short_description = 'foto'
+   
+   list_display = ("rodzaj", "opis", "foto", 'image_tag',)
     
     
   
