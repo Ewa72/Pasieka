3b802ce31ac39ed9eabc1ae9da7eb4c9932ad319
Ewa72 <ewa07adamus@gmail.com>
2024-10-10 14:43:15 +0200

    10.10.24

--- Pasieka/settings.py
+++ Pasieka/settings.py
@@ -145,8 +145,8 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = '/media/'
 #MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/media/'
 
 
 # Update database configuration from $DATABASE_URL environment variable (if defined)
