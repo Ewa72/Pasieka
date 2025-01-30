453378e8c9b1a204a0d027acec42fd097be9ea36
Ewa72 <ewa07adamus@gmail.com>
2025-01-30 14:48:34 +0100

    miody 30.01

--- Produkty/urls.py
+++ Produkty/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     #path('miody/', views.miody, name='miody'),
-    path('Produkty/miody/', views.miody, name='miody'),
-    path('miody/', views.miody, name='miody'),
-    path('miod/<int:pk>',views.MiodyDetailView.as_view(), name='miod-detail'),
+   # path('Produkty/miody/', views.miody, name='miody'),
+   # path('miody/', views.miody, name='miody'),
+    #path('miod/<int:pk>',views.MiodyDetailView.as_view(), name='miod-detail'),
+    path('miody/', views.MiodyListView.as_view(), name='miody'),
 ]
